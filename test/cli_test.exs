2450234c95658f360cfb33536f36be0cd7770881
GrivJ <jgriveau@yahoo.fr>
2025-10-27 09:51:05 +0100

    feat(cli): :sparkles: add cli parsing and test suit

---
+++ test/cli_test.exs
@@ -0,0 +1,19 @@
+defmodule Issues.CLI.Test do
+  use ExUnit.Case
+  doctest Issues.CLI
+
+  import Issues.CLI
+
+  test ":help is returned if help is given (as -h or --help)" do
+    assert parse_args(["-h", "user", "project"]) == :help
+    assert parse_args(["--help", "anything"]) == :help
+  end
+
+  test "three values are returned if three values given" do
+    assert parse_args(["user", "project", "10"]) == {"user", "project", 10}
+  end
+
+  test "two values are returned if two values given (default count is used)" do
+    assert parse_args(["user", "project"]) == {"user", "project", 4}
+  end
+end
